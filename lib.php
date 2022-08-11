e4ccdfc7e9af5af27abec532c4783fd1e396836c
Willian Mano <willianmanoaraujo@gmail.com>
2022-08-11 17:03:04 -0300

    Fixes download file

--- lib.php
+++ lib.php
@@ -213,3 +213,41 @@ function mod_cfp_output_fragment_question_form($args) {
 
     return $o;
 }
+
+/**
+ * Serves the files from the mod_cfp file areas.
+ *
+ * @package     mod_cfp
+ * @category    files
+ *
+ * @param stdClass $course The course object.
+ * @param stdClass $cm The course module object.
+ * @param stdClass $context The mod_cfp's context.
+ * @param string $filearea The name of the file area.
+ * @param array $args Extra arguments (itemid, path).
+ * @param bool $forcedownload Whether or not force download.
+ * @param array $options Additional options affecting the file serving.
+ */
+function cfp_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload, $options = array()) {
+    if ($context->contextlevel != CONTEXT_MODULE) {
+        send_file_not_found();
+    }
+
+    require_login($course, false, $cm);
+
+    $itemid = (int)array_shift($args);
+    if ($itemid == 0) {
+        return false;
+    }
+
+    $relativepath = implode('/', $args);
+
+    $fullpath = "/{$context->id}/mod_cfp/$filearea/$itemid/$relativepath";
+
+    $fs = get_file_storage();
+    if (!$file = $fs->get_file_by_hash(sha1($fullpath)) or $file->is_directory()) {
+        return false;
+    }
+
+    send_stored_file($file, 0, 0, $forcedownload, $options);
+}
